11a28c66fb0ddaf37533302d325054550bca8e20
hierich <506673825@qq.com>
2019-06-01 17:27:20 +0800

    seperate virtual_struct and virtual_plantables

--- scripts/prefabs/virtualplantable.lua
+++ scripts/prefabs/virtualplantable.lua
@@ -19,11 +19,9 @@ local function make_virtualitem(data)
 				deployer.SoundEmitter:PlaySound("dontstarve/common/plant")             
 			end         
 		end     
-	end      
+	end
 
-     
-
-	local function fn()         
+	local function itemfn()         
 		local inst = CreateEntity()          
 		inst.entity:AddTransform()         
 		inst.entity:AddAnimState()         
@@ -55,26 +53,78 @@ local function make_virtualitem(data)
 		return inst     
 	end      
 
+    local function onhammered(inst, worker)         
+		inst:Remove()     
+	end      
 
-	return Prefab("virtualdug_"..data.name, fn, assets)
+	local function CanActivate(inst, doer)                  
+		local ret = false         
+            
+		ret = doer.components.inventory:Has("dug_"..name,1)                
+		return ret     
+	end      
+
+	local function Activate(inst, doer)                  
+		local truestruct = SpawnPrefab(name)         
+		doer.components.inventory:ConsumeByName("dug_"..name, 1)             
+		truestruct.Transform:SetPosition(inst.Transform:GetWorldPosition())             
+		truestruct.components.pickable:OnTransplant()	        		         
+		onhammered(inst)     
+	end 
+
+	local function fn()         
+		local inst = CreateEntity()          
+		inst.entity:AddTransform()         
+		inst.entity:AddAnimState()         
+		inst.entity:AddNetwork()          
+		inst:AddTag("structure")          
+		inst.AnimState:SetBank(data.bank)         
+		inst.AnimState:SetBuild(data.build)         
+		inst.AnimState:PlayAnimation(data.anim)
+		         
+		inst.AnimState:SetMultColour(TUNING.VIRTUALRED,TUNING.VIRTUALGREEN,TUNING.VIRTUALBLUE,TUNING.VIRTUALALPHA)          
+		inst.entity:SetPristine()          
+
+		if not TheWorld.ismastersim then             
+			return inst         
+		end          
+
+		inst:AddComponent("workable")         
+		inst.components.workable:SetWorkAction(ACTIONS.HAMMER)         
+		inst.components.workable:SetWorkLeft(1)         
+		inst.components.workable:SetOnFinishCallback(onhammered)         
+		inst:AddComponent("activatable")         
+		inst.components.activatable.quickaction = true         
+		inst.components.activatable.CanActivateFn = CanActivate         
+		inst.components.activatable.OnActivate = Activate   
+
+		return inst     
+	end      
+
+
+	return Prefab("virtual"..data.name, fn, assets),
+		Prefab("virtualdug_"..data.name, itemfn, assets),
+		MakePlacer("virtualdug_"..data.name.."_placer", data.bank or data.name, data.build or data.name, data.anim or "idle")
 	
 end  
 
 local plantables = 
 {
-	{name = "berrybush",anim = "dead",},
-	{name = "berrybush2",anim = "dead",inspectoverride = "dug_berrybush",},
-	{name = "berrybush_juicy",anim = "dead",inspectoverride = "dug_berrybush",},
-	{name = "sapling",mediumspacing = true     },     
-	{name = "grass",build = "grass1",mediumspacing = true     },     
-	{name = "marsh_bush",mediumspacing = true     }, 
+	{name = "berrybush", bank = "berrybush", build = "berrybush", anim = "idle",},
+	{name = "berrybush2", bank = "berrybush2", build = "berrybush2", anim = "idle",},
+	{name = "berrybush_juicy", bank = "berrybush_juicy", build = "berrybush_juicy", anim = "idle",},
+	{name = "sapling", bank = "sapling", build = "sapling", anim = "idle", mediumspacing = true,},     
+	{name = "grass", bank = "grass", build = "grass1", anim = "idle", mediumspacing = true,},     
+	{name = "marsh_bush", bank = "marsh_bush", build = "marsh_bush", anim = "idle", mediumspacing = true,}, 
 }  
 
 local prefabs = {} 
 
-for i, v in ipairs(plantables) do     
-	table.insert(prefabs, make_virtualitem(v))     
-	table.insert(prefabs, MakePlacer("virtualdug_"..v.name.."_placer", v.bank or v.name, v.build or v.name, v.anim or "idle")) 
+for i, v in ipairs(plantables) do
+	local plant, dug_plant, placer = make_virtualitem(v)
+	table.insert(prefabs, plant)     
+	table.insert(prefabs, dug_plant)
+	table.insert(prefabs, placer) 
 end  
 
 
